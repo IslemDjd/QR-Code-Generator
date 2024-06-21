e422b6d37c929971584e3284c730928c15c60bc6
Islem Dj <islemas23@gmail.com>
2024-06-21 23:03:29 +0100

    Init

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'outfit': ['outfit', 'sans-serif'],
+        'mdMed': ['mdMed', 'sans-serif'],
+        'mdReg': ['mdReg', 'sans-serif'],
+        'mdSemi': ['mdSemi', 'sans-serif'],
+        'mdBold': ['mdBold', 'sans-serif'],
+        'BrandFont': ['BrandFont', 'sans-serif'],
+        'afacad': ['afacad', 'sans-serif'],
+        'kanit': ['kanit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
